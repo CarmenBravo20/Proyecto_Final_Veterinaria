cf10996744226b6db2632424fecace0dbd49ecfe
CarmenBravo20 <cbravov1@est.ups.edu.ec>
2022-01-15 13:41:14 -0500

    Creacion del paquete modelo con la clase cliente y mascota

---
+++ VeterinariaServidor/src/main/java/ec/edu/ups/pweb/CLIENTE/DAO/ClienteDAO.java
@@ -0,0 +1,37 @@
+package ec.edu.ups.pweb.CLIENTE.DAO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import ec.edu.ups.pweb.CLIENTE.MODEL.Cliente;
+
+@Stateless
+public class ClienteDAO {
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	public void insert(Cliente op) {
+		em.persist(op);		
+	}
+	
+	public void update(Cliente op) {
+		em.merge(op);
+	}
+	
+	public Cliente read(String cedula) {
+		Cliente op = em.find(Cliente.class, cedula);
+		return op;
+	}
+	
+	public void delete( String cedula) {
+		Cliente op = em.find(Cliente.class, cedula);
+		em.remove(op);
+	}
+
+}
---
+++ VeterinariaServidor/src/main/java/ec/edu/ups/pweb/CLIENTE/MODEL/Cliente.java
@@ -0,0 +1,66 @@
+package ec.edu.ups.pweb.CLIENTE.MODEL;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TBL_Cliente")
+public class Cliente implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L; 
+	
+		@Id
+		@Column(name="cli_cedula")
+		private String cedula;
+		
+		@Column(name="cli_nombre")
+		private String nombre;
+		
+		@Column(name="cli_direccion")
+		private String direccion;
+		
+		@Column(name="cli_telefono")
+		private String telefono;
+
+		public String getCedula() {
+			return cedula;
+		}
+
+		public void setCedula(String cedula) {
+			this.cedula = cedula;
+		}
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public String getDireccion() {
+			return direccion;
+		}
+
+		public void setDireccion(String direccion) {
+			this.direccion = direccion;
+		}
+
+		public String getTelefono() {
+			return telefono;
+		}
+
+		public void setTelefono(String telefono) {
+			this.telefono = telefono;
+		}
+		
+
+		
+
+}
